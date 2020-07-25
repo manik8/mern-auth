1eafb62286f57177131171944334448748215b83
manik8 <maniksharma2014.ms@gmail.com>
2020-07-25 20:11:43 +0530

    Backend Completed

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../model/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 
 router.post("/register", async (req, res) => {
   try {
@@ -80,6 +81,38 @@ router.post("/login", async (req, res) => {
       },
     });
     console.log(token);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const deleteUser = await User.findByIdAndDelete(req.user);
+    res.json(deleteUser);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/tokenIsValid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res.json(false);
+    }
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) {
+      return res.json(false);
+    }
+    const user = await User.findById(verified.id);
+    if (!user) {
+      return res.json(false);
+    }
+
+    return res.json(true);
   } catch (err) {
     console.error(err.message);
     res.status(500).json(err);
